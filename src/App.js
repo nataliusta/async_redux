2e9d54ce89852083893afaaed172afb138fa81e8
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-09-05 19:15:19 +0300

    Create a GET request

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
@@ -6,6 +7,14 @@ import Products from './components/Shop/Products';
 
 function App() {
   const isShown = useSelector(state => state.ui.cartIsShown);
+  const cart = useSelector(state => state.cart);
+
+  useEffect(() => {
+    fetch('https://food-react-e2f74-default-rtdb.europe-west1.firebasedatabase.app/cart.json', { 
+      method: 'PUT',
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
 
   return (
     <Layout>
--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -11,45 +11,17 @@ const ProductItem = (props) => {
   const { title, price, description, id } = props;
 
   const addToCartHandler = () => {
-    const newTotalQuantity = cart.totalQuantity + 1;
-
-    const updatedItems = cart.items.slice(); // create copy via slice to avoid mutating original state
-    const existingItem = updatedItems.find((item) => item.id === id);
-    if (existingItem) {
-      const updatedItem = { ...existingItem }; // new object + copy existing properties to avoid state mutation
-      updatedItem.quantity++;
-      updatedItem.totalPrice = updatedItem.totalPrice + price;
-      const existingItemIndex = updatedItems.findIndex(
-        (item) => item.id === id
-      );
-      updatedItems[existingItemIndex] = updatedItem;
-    } else {
-      updatedItems.push({
-        id: id,
-        price: price,
-        quantity: 1,
-        totalPrice: price,
-        name: title,
-      });
-    }
-
-    const newCart = {
-      totalQuantity: newTotalQuantity,
-      items: updatedItems,
-    };
-
-    dispatch(cartActions.replaceCart(newCart));
 
     // and then send Http request
     // fetch('firebase-url', { method: 'POST', body: JSON.stringify(newCart) })
 
-    // dispatch(
-    //   cartActions.addItemToCart({
-    //     id,
-    //     title,
-    //     price,
-    //   })
-    // );
+    dispatch(
+       cartActions.addItemToCart({
+         id,
+         title,
+         price,
+       })
+    );
   };
 
   return (
