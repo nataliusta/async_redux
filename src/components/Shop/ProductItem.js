9cbfeba2746ecdf6d497ee24d8913cd5f4bea640
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-09-05 20:27:16 +0300

    Add Notification files

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,11 +1,10 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import { cartActions } from '../../store/cart-slice';
 import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
 
 const ProductItem = (props) => {
-  const cart = useSelector((state) => state.cart);
   const dispatch = useDispatch();
 
   const { title, price, description, id } = props;
