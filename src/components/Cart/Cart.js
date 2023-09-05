c39385b6abcc2f3bbff97c41d9c252e8de309fb1
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-09-05 18:24:53 +0300

    Add logic to CartItem.js

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -14,7 +14,13 @@ const Cart = (props) => {
         {cartItems.map((item) => (
           <CartItem
             key={item.id}
-            item={{ title: item.name, quantity: item.quantity, total: item.totalPrice, price: item.price }}
+            item={{ 
+              id: item.id,
+              title: item.name, 
+              quantity: item.quantity, 
+              total: item.totalPrice, 
+              price: item.price 
+            }}
           />
         ))}
       </ul>
--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,7 +1,23 @@
+import { useDispatch } from 'react-redux';
+
 import classes from './CartItem.module.css';
+import { cartActions } from '../../store/cart-slice';
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+  const dispatch = useDispatch();
+  const { title, quantity, total, price, id } = props.item;
+
+  const removeItemHandle = () => {
+    dispatch(cartActions.removeItemFromCart(id));
+  };
+
+  const addItemHandle = () => {
+    dispatch(cartActions.addItemToCart({
+      id,
+      title,
+      price,
+    }));
+  };
 
   return (
     <li className={classes.item}>
@@ -17,8 +33,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={removeItemHandle}>-</button>
+          <button onClick={addItemHandle}>+</button>
         </div>
       </div>
     </li>
