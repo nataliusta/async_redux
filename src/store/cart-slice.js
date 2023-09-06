e1462285c333d1bfe9fccafb7464a836726f37f5
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-09-06 17:10:45 +0300

    Update cart-slice.js

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -40,7 +40,7 @@ const cartSlice = createSlice({
     },
 });
 
-const sendCartData = (cart) => {
+export const sendCartData = (cart) => {
     return async (dispatch) => {
         dispatch(
             uiActions.showNotification({
