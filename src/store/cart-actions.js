d8419e294a9913fc7412236d5c2430100a2c46a7
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-09-06 19:59:11 +0300

    Update cart-actions.js

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -17,7 +17,10 @@ export const fetchCartData = () => {
 
         try {
             const cartData = await fetchData();
-            dispatch(cartActions.replaceCart(cartData));
+            dispatch(cartActions.replaceCart({
+                items: cartData.items || [],
+                totalQuantity: cartData.totalQuantity,
+            }));
         } catch (error) {
             dispatch(
                 uiActions.showNotification({
