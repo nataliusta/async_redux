5b07f37f97bb566279af1c6dd0c60f4dc6eb3d20
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-09-06 16:34:36 +0300

    Create Action Creator

--- src/App.js
+++ src/App.js
@@ -17,20 +17,6 @@ function App() {
 
   useEffect(() => {
     const sendCartData = async () => {
-      dispatch(uiActions.showNotification({
-          status: 'pending',
-          title: 'Sending..',
-          message: 'Sending cart data'
-        })
-      );
-        const response = await fetch('https://food-react-e2f74-default-rtdb.europe-west1.firebasedatabase.app/cart.json', { 
-        method: 'PUT',
-        body: JSON.stringify(cart),
-      });
-
-      if (!response.ok) {
-        throw new Error('Sending cart data failed!');
-      }  
 
         dispatch(uiActions.showNotification({
           status: 'success',
