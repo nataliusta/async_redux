bef68c9b3701f18e32aa1b46aadaa7eddcd3076d
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-09-06 19:44:54 +0300

    Add fetching cart data

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,5 +1,4 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { uiActions } from './ui-slice';
 
 const cartSlice = createSlice({
     name: 'cart',
@@ -39,50 +38,6 @@ const cartSlice = createSlice({
         },
     },
 });
-
-export const sendCartData = (cart) => {
-    return async (dispatch) => {
-        dispatch(
-            uiActions.showNotification({
-            status: 'pending',
-            title: 'Sending..',
-            message: 'Sending cart data'
-          })
-        );
-
-        const sendRequest = async () => {
-            const response = await fetch('https://food-react-e2f74-default-rtdb.europe-west1.firebasedatabase.app/cart.json', { 
-                method: 'PUT',
-                body: JSON.stringify(cart),
-              }
-            );
-        
-            if (!response.ok) {
-                throw new Error('Sending cart data failed!');
-            } 
-        };
-
-        try {
-            await sendRequest();
-
-            dispatch(
-                uiActions.showNotification({
-                    status: 'success',
-                    title: 'Success!',
-                    message: 'Sent cart data successfully!'
-                })
-            );
-        } catch (error) {
-            dispatch(
-                    uiActions.showNotification({
-                    status: 'error',
-                    title: 'Error!',
-                    message: 'Sending cart data failed!'
-                })
-            );
-        }
-    };
-};
 
 export const cartActions = cartSlice.actions;
 export default cartSlice;
